148116fc00f7241f886b2bbe8da761f178c2453c
Aditya <adityabasak7031@gmail.com>
2025-02-24 19:26:48 +0530

    navbar and hero done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,10 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import type { Config } from "tailwindcss";
+// const colors = require("tailwindcss/colors");
+// const {
+//   default: flattenColorPalette,
+
+// } = require("tailwindcss/lib/util/flattenColorPalette")
 
 export default {
   content: [
@@ -6,11 +12,29 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            // @ts-ignore
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            // @ts-ignore
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
       },
     },
   },
